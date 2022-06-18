d7f8a353afb58c9d3c2068819636206e781dd263
Dr. Christian De Britto <admin@socioatlas.xyz>
2022-06-18 18:40:24 -0300

    Retrieve sheet link from URL parameter

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tMx_0t1HgMtcHO-YSyfTYnvkbM7M1uJaM9d95TzTozg/edit#gid=0';
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tMx_0t1HgMtcHO-YSyfTYnvkbM7M1uJaM9d95TzTozg/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyDfaumHeyaFS1vvmJKcIlKHNqCkNlGDBSE';
